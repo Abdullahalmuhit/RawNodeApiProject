b3a9aa46e341cd9914e3fae9ddfa0044a4287a08
Muhid <muhid@ihealthscreen.org>
2022-06-30 16:49:49 +0600

    Added response

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 const http = require("http");
 const url = require('url');
+const {StringDecoder} = require('string_decoder'); 
 
 const app = {};
 
@@ -24,13 +25,30 @@ app.ServerCreate = () => {
 };
 
 app.handleReqRes = (req, res) => {
-  res.end("Hello World ! From Muhid");
+  // res.end("Hello World ! From Muhid");
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const treamPath = path.replace(/^\/+|\/+$/g,'');
   const method=req.method.toLowerCase();
   const queryStringObject = parsedUrl.query;
-  console.log(queryStringObject);
+  const headersObject = req.headers;
+  const decoder = new StringDecoder('utf-8');
+  let realData='';
+
+  req.on('data', (buffer)=>{
+    realData += decoder.write(buffer);
+
+  });
+  req.on('end', ()=>{
+    realData += decoder.end();
+    console.log(realData);
+    console.log('hello muhit bhai');
+    res.end(realData);
+
+
+  });
+
+
 };
 app.ServerCreate();
 
